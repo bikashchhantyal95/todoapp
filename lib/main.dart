e5e48e14e74e43fae2889c988190f088e430badf
Bikash Chhantyal <bikashchhantyal2@gmail.com>
2025-01-27 16:14:42 -0500

    feat(todo): add static To-Do List UI with fake tasks

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todoapp/screens/todo_home_screen.dart';
 import 'package:todoapp/splash_screen.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SplashScreen(),
+      home: const TodoHomeScreen(),
     );
   }
 }
--- lib/screens/todo_home_screen.dart
+++ lib/screens/todo_home_screen.dart
@@ -8,12 +8,36 @@ class TodoHomeScreen extends StatefulWidget {
 }
 
 class _TodoHomeScreenState extends State<TodoHomeScreen> {
+  final List _todos = [
+    {'title': 'Buy groceries', 'isCompleted': false},
+    {'title': 'Finish Flutter project', 'isCompleted': true},
+    {'title': 'Call the plumber', 'isCompleted': false},
+    {'title': 'Go to the gym', 'isCompleted': true},
+    {'title': 'Prepare dinner', 'isCompleted': false},
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Todo"),
       ),
+      body: Expanded(
+          child: ListView.builder(
+              itemCount: _todos.length,
+              itemBuilder: (context, index) {
+                final todo = _todos[index];
+                return ListTile(
+                  leading:
+                      Checkbox(value: todo['isCompleted'], onChanged: null),
+                  title: Text(
+                    todo['title'] as String,
+                    style: TextStyle(
+                        decoration: (todo['isCompleted'] as bool)
+                            ? TextDecoration.lineThrough
+                            : TextDecoration.none),
+                  ),
+                );
+              })),
     );
   }
 }
